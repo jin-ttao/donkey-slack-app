2b357e6eff327edfaca6a3ead177003d3abd0390
Jintae Song <w.jinttao@gmail.com>
2025-04-05 23:41:58 +0900

    fix mongodb notation

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { CONFIG } from './index';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(CONFIG.MONGODB_URI);
+    const conn = await mongoose.connect(CONFIG.MONGODB.URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
